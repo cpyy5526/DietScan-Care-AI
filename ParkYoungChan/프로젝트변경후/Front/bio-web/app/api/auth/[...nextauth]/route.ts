9a2566bfea37958ed8c6a64347b1eccf1afa7363
Youngchan <lgihtyear@gmail.com>
2024-12-14 02:24:12 +0900

    사용자 db 관리 방식 변경 csv 파일 대신 SQLite db파일로 변경, 그에 따른 prisma 및 db api접근 코드 수정

--- ParkYoungChan/프로젝트변경후/Front/bio-web/app/api/auth/[...nextauth]/route.ts
+++ ParkYoungChan/프로젝트변경후/Front/bio-web/app/api/auth/[...nextauth]/route.ts
@@ -1,52 +1,6 @@
-import prisma from '@/lib/prisma';
-import {PrismaAdapter} from '@next-auth/prisma-adapter';
-import bcrypt from 'bcrypt';
-import NextAuth, {AuthOptions} from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: {label: 'Email', type: 'text'},
-        password: {label: 'Password', type: 'password'},
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error('Invalid credentials');
-        }
-
-        const user = await prisma.user.findUnique({
-          where: {email: credentials.email},
-        });
-
-        if (!user || !user.password) {
-          throw new Error('User not found');
-        }
-
-        const isValidPassword =
-            await bcrypt.compare(credentials.password, user.password);
-
-        if (!isValidPassword) {
-          throw new Error('Invalid password');
-        }
-
-        return {id: user.id, email: user.email, name: user.name};
-      },
-    }),
-  ],
-  pages: {
-    signIn: '/login',
-    signOut: '/',
-    error: '/login',  // 오류 시 로그인 페이지로 이동
-  },
-  session: {
-    strategy: 'jwt',  // 정확히 설정
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import {authOptions} from '@/lib/auth';
+import NextAuth from 'next-auth';
 
 const handler = NextAuth(authOptions);
+
 export {handler as GET, handler as POST};
