ac139fb57987ed7b7ca90c7aae48f2587935d96c
Youngchan <lgihtyear@gmail.com>
2024-12-14 02:05:27 +0900

    수소이온, TDS 데이터를 통계수치만 표시하는 component 추가 및 메인 대시보드에 import

---
+++ ParkYoungChan/프로젝트변경후/Front/bio-web/components/variance.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { fetchData } from '@/app/api/fetchData';
+
+export default function Statistical({ sensor_name, sensor_value, what_value, day }) {
+  const [oxygenValues, setOxygenValues] = useState([]); // 데이터를 저장할 상태
+
+  useEffect(() => {
+    const loadData = async () => {
+      const data = await fetchData(sensor_name, sensor_value, day);
+
+      // 데이터 순서를 반대로 (가장 오래된 시간이 왼쪽, 최신 시간이 오른쪽)
+      const reversedData = data.slice().reverse();
+
+      // Y축 값 (pH 값) 추출
+      const oxygenValues = reversedData.map((item) => item[what_value].value);
+
+      // 상태에 저장
+      setOxygenValues(oxygenValues);
+    };
+
+    loadData();
+  }, [sensor_name, sensor_value, what_value]);
+
+  return <div>{variance(oxygenValues).toFixed(4)}</div>;
+}
+function add(values) {
+  return values.reduce((acc, curr) => acc + curr, 0);
+}
+
+// 분산 계산 함수
+function variance(values) {
+  const mean = add(values) / values.length; // 평균 계산
+  return values.reduce((acc, curr) => acc + Math.pow(curr - mean, 2), 0); // 분산 계산
+}
