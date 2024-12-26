7f32005a545c9c1a0912b12745c67c9bb51a3419
kms <cpyy5526@knu.ac.kr>
2024-12-26 18:47:07 +0900

    FastAPI의 비동기 호출을 적용 가능한 부분을 모두 찾고, 함수 정의 및 호출을 전반적으로 수정

--- Minseok-Kim/소스코드/bfdetect-backend/database.py
+++ Minseok-Kim/소스코드/bfdetect-backend/database.py
@@ -1,14 +1,13 @@
-import sqlite3
+import aiosqlite
 import datetime
 
 # 데이터베이스 경로
 DB_PATH = "/app/data/results.db"
 
-def initialize():
-    with sqlite3.connect(DB_PATH) as conn:
-        cursor = conn.cursor()
+async def initialize():
+    async with aiosqlite.connect(DB_PATH) as conn:
         # 결과 저장 테이블 생성
-        cursor.execute("""
+        await conn.execute("""
         CREATE TABLE IF NOT EXISTS judge_results (
             id INTEGER        PRIMARY KEY  AUTOINCREMENT,
             sensor_id         TEXT         NOT NULL,
@@ -16,51 +15,45 @@ def initialize():
             inference_result  INTEGER
         );
         """)
-        conn.commit()
-
+        await conn.commit()
 
 # 결과 저장
-def saveResult(sensor_id: str, inference_result: str):
-    with sqlite3.connect(DB_PATH) as conn:
-        cursor = conn.cursor()
-        cursor.execute("""
+async def saveResult(sensor_id: str, inference_result: str):
+    async with aiosqlite.connect(DB_PATH) as conn:
+        await conn.execute("""
         INSERT INTO judge_results (sensor_id, inference_time, inference_result)
         VALUES (?, ?, ?);
         """, (sensor_id, datetime.datetime.now(), inference_result))
-        conn.commit()
-
+        await conn.commit()
 
 # 특정 센서의 가장 최근 결과 조회
-def getLatestResult(sensor_id: str):
-    with sqlite3.connect(DB_PATH) as conn:
-        cursor = conn.cursor()
-        cursor.execute("""
+async def getLatestResult(sensor_id: str):
+    async with aiosqlite.connect(DB_PATH) as conn:
+        cursor = await conn.execute("""
         SELECT *
         FROM judge_results
         WHERE sensor_id = ?
         ORDER BY inference_time DESC
         LIMIT 1;
         """, (sensor_id,))
-        result = cursor.fetchone()
+        result = await cursor.fetchone()
     return {
         "sensor_id": result[1],
         "inference_time": result[2],
         "inference_result": result[3],
     }
 
-
 # 특정 센서의 과거 시점 결과 조회
-def getResultsByTime(sensor_id: str, start_time: str, end_time: str):
-    with sqlite3.connect(DB_PATH) as conn:
-        cursor = conn.cursor()
-        cursor.execute("""
+async def getResultsByTime(sensor_id: str, start_time: str, end_time: str):
+    async with aiosqlite.connect(DB_PATH) as conn:
+        cursor = await conn.execute("""
         SELECT *
         FROM judge_results
         WHERE sensor_id = ?
               AND inference_time BETWEEN ? AND ?
         ORDER BY inference_time;
         """, (sensor_id, start_time, end_time))
-        results = cursor.fetchall()
+        results = await cursor.fetchall()
     return [
         {
             "sensor_id": row[1],
--- Minseok-Kim/소스코드/bfdetect-backend/main.py
+++ Minseok-Kim/소스코드/bfdetect-backend/main.py
@@ -5,7 +5,7 @@ from typing import List, Optional
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.executors.asyncio import AsyncIOExecutor
 from apscheduler.triggers.cron import CronTrigger
-import os, asyncio, logging, requests
+import os, logging, requests
 
 SENSOR_IDS = ["wando01", "wando02", "wando01b"]
 
@@ -28,7 +28,6 @@ logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 logger.setLevel(logging.INFO)
 
-
 # 센서 하나에 대해 바이오파울링 예측 모델 실행
 # return: 0(normal) / 1(abnomarl)
 async def runModel(sensor_id: str):
@@ -36,7 +35,7 @@ async def runModel(sensor_id: str):
         # 데이터 수집
         try:
             # data = pipeline.collect_recent_data(sensor_id, app.state.token)
-            data = pipeline.collect_recent_data(sensor_id, None)
+            data = await pipeline.collect_recent_data(sensor_id, None)
             logger.info(f"\'{sensor_id}\': Collected data from the server.")
         except requests.exceptions.RequestException as e:
             logger.error(f"API request error: {e}")
@@ -51,31 +50,33 @@ async def runModel(sensor_id: str):
         # 모델 로드
         model, device = pipeline.load_model("model.pth")
 
-        # 예측 및 저장
+        # 예측
         result = pipeline.predict_anomaly(model, processed_data, device)  
         logger.info(f"\'{sensor_id}\': Prediction has been successfully completed.")
-        database.saveResult(sensor_id, 1 if result == 1 else 0)
+
+        # 결과 저장
+        await database.saveResult(sensor_id, 1 if result == 1 else 0)
 
     except Exception as e:
         logger.error(str(e), exc_info=True)
         logger.info("Prediction for \'{sensor_id}\' failed.")
         return None
-
+async def runModelWrapper(sensor_id: str):
+    await runModel(sensor_id)
 
 # 서버 시작
 @app.on_event("startup")
 async def start_scheduler():
     try:
-        database.initialize()
+        await database.initialize()
         # app.state.token = pipeline.get_access_token()  # 오든 API 토큰 획득
 
         # 1시간마다 모든 센서에 대해 모델을 실행하도록 스케줄링
         scheduler = AsyncIOScheduler(executors={"default": AsyncIOExecutor()})
-        trigger = CronTrigger(minute="*/1")   # for test
-        # trigger = CronTrigger(minute="0")   # 정각(##:00)마다 실행
+        # trigger = CronTrigger(minute="*/1")   # for test
+        trigger = CronTrigger(minute="0")   # 정각(##:00)마다 실행
         for sensor_id in SENSOR_IDS:
-            scheduler.add_job(runModel, trigger, args=[sensor_id],
-                              id=f"job_{sensor_id}", max_instances=3)
+            scheduler.add_job(runModelWrapper, trigger, args=[sensor_id], id=f"job_{sensor_id}", max_instances=3)
         scheduler.start()
         logger.info("Scheduler started.")
     
@@ -90,31 +91,29 @@ class Result(BaseModel):
 
 # 센서의 가장 최근 바이오파울링 판단 결과를 DB에서 가져옴
 @app.get("/results/latest/{sensor_id}", response_model=Result)
-def latestResult(sensor_id: str):
+async def latestResult(sensor_id: str):
     try:
-        result = database.getLatestResult(sensor_id)
-        logger.info(f"Fetched latest result for \'{sensor_id}\'")
+        result = await database.getLatestResult(sensor_id)
+        logger.info(f"Fetched the latest result for \'{sensor_id}\'")
         return Result(**result)
     except Exception as e:
         logger.error(f"An error occured fetching latest result for \'{sensor_id}\': {e}", exc_info=True)
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
-
 # 주어진 기간 동안의 과거 판단 결과를 DB에서 가져옴
 @app.get("/results/range/{sensor_id}", response_model=List[Result])
-def resultsByTime(sensor_id: str, start_time: str, end_time: str):
+async def resultsByTime(sensor_id: str, start_time: str, end_time: str):
     try:
-        results = database.getResultsByTime(sensor_id, start_time, end_time)
+        results = await database.getResultsByTime(sensor_id, start_time, end_time)
         logger.info(f"Fetched results for \'{sensor_id}\' from {start_time} to {end_time}.")
         return [Result(**result) for result in results]
     except Exception as e:
         logger.error(f"An error occured fetching results for \'{sensor_id}\': {e}", exc_info=True)
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
-
 # 오든 센서 데이터 API에 대한 Proxy API
 @app.get("/proxy/{endpoint}")
-async def proxy_request(endpoint: str, params: dict = None):
+def proxy_request(endpoint: str, params: dict = None):
     # headers = {"Authorization": f"Bearer {app.state.token}"}
     headers = {"Content-Type": "application/json"}
     url = f"{os.getenv('API_BASE_URL')}/{endpoint}"
