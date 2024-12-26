9168ca20d8c773253f430c66c2298ab252139fdc
kms <cpyy5526@knu.ac.kr>
2024-12-26 18:57:25 +0900

    데이터가 이미 비정상적으로 수집되는 것이 명백한 경우, AI 모델을 실행하지 않고 별도 필드에 기록하도록 수정

--- Minseok-Kim/소스코드/bfdetect-backend/database.py
+++ Minseok-Kim/소스코드/bfdetect-backend/database.py
@@ -9,22 +9,25 @@ async def initialize():
         # 결과 저장 테이블 생성
         await conn.execute("""
         CREATE TABLE IF NOT EXISTS judge_results (
-            id INTEGER        PRIMARY KEY  AUTOINCREMENT,
+            id                INTEGER      PRIMARY KEY  AUTOINCREMENT,
             sensor_id         TEXT         NOT NULL,
-            inference_time    DATETIME     NOT NULL,
-            inference_result  INTEGER
+            timestamp         DATETIME,     
+            data_abnormal     INTEGER,
+            predict_result    INTEGER
         );
         """)
         await conn.commit()
 
+
 # 결과 저장
-async def saveResult(sensor_id: str, inference_result: str):
+async def saveResult(sensor_id: str, predict_result: int, data_abnormal: int=0):
     async with aiosqlite.connect(DB_PATH) as conn:
         await conn.execute("""
-        INSERT INTO judge_results (sensor_id, inference_time, inference_result)
-        VALUES (?, ?, ?);
-        """, (sensor_id, datetime.datetime.now(), inference_result))
+        INSERT INTO judge_results (sensor_id, data_abnormal, timestamp, predict_result)
+        VALUES (?, ?, ?, ?);
+        """, (sensor_id, data_abnormal, datetime.datetime.now(), predict_result))
         await conn.commit()
+
 
 # 특정 센서의 가장 최근 결과 조회
 async def getLatestResult(sensor_id: str):
@@ -40,8 +43,9 @@ async def getLatestResult(sensor_id: str):
     return {
         "sensor_id": result[1],
         "inference_time": result[2],
-        "inference_result": result[3],
+        "inference_result": 1 if result[3] == 1 else result[4]
     }
+
 
 # 특정 센서의 과거 시점 결과 조회
 async def getResultsByTime(sensor_id: str, start_time: str, end_time: str):
@@ -58,7 +62,7 @@ async def getResultsByTime(sensor_id: str, start_time: str, end_time: str):
         {
             "sensor_id": row[1],
             "inference_time": row[2],
-            "inference_result": row[3],
+            "inference_result": 1 if row[3] == 1 else row[4]
         }
         for row in results
     ]
