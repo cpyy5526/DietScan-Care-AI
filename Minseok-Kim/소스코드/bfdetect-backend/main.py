eb39e8073cfeffc5e1dcb361a89158ec2eee1efb
kms <cpyy5526@knu.ac.kr>
2024-12-26 19:07:30 +0900

    시연을 위해 과거 데이터에 대한 AI 모델 수행 로직 추가, 도입기업 API 호출 시 과부하 방지 및 안정성을 위한 추가 예외 처리 추가

--- Minseok-Kim/소스코드/bfdetect-backend/main.py
+++ Minseok-Kim/소스코드/bfdetect-backend/main.py
@@ -1,15 +1,15 @@
 import pipeline, database
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, BackgroundTasks
 from pydantic import BaseModel
 from typing import List, Optional
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.executors.asyncio import AsyncIOExecutor
 from apscheduler.triggers.cron import CronTrigger
 from datetime import datetime, timedelta
-import os, logging, requests
+import os, logging, asyncio
 
 SENSOR_IDS = ["wando01", "wando02", "wando01b"]
-PAST_DATA = os.getenv("PAST_DATA", "false").lower() == "true"
+semaphore = asyncio.Semaphore(1)
 
 app = FastAPI()
 
@@ -21,8 +21,8 @@ logging.basicConfig(level=logging.INFO)
 console_handler = logging.StreamHandler()
 file_handler = logging.FileHandler("/app/data/main.log")
 
-console_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
-file_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+console_formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+file_formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
 console_handler.setFormatter(console_formatter)
 file_handler.setFormatter(file_formatter)
 
@@ -33,25 +33,27 @@ logger.setLevel(logging.INFO)
 
 # 센서 하나에 대해 바이오파울링 예측 모델 실행
 # return: 0(normal) / 1(abnomarl)
-async def runModel(sensor_id: str, time: datetime):
+async def runModel(sensor_id: str, time: Optional[datetime]=None):
     try:
-        # 데이터 수집
-        try:
-            data = await pipeline.collect_recent_data(sensor_id, time)
-            logger.info(f"\'{sensor_id}\': Collected data from the server.")
-        except requests.exceptions.RequestException as e:
-            logger.error(f"API request error: {e}")
-        except ValueError as e:
-            logger.error(f"Data processing error: {e}")
+        if time is None:
+            time = datetime.now()
+            time = time.replace(minute=0, second=0, microsecond=0)
+        logger.info(f"[\'{sensor_id}\' at {time}] Starting prediction...")
+
+        # 데이터 수집. 병렬 처리 시 오든 API에 과부하를 방지하기 위해 동시에 하나만 실행되도록 제한.
+        async with semaphore:
+            data = await pipeline.collect_data(sensor_id, time)
+            logger.info(f"[\'{sensor_id}\' at {time}] Collected data from the server.")
+            await asyncio.sleep(1)
 
         # 데이터 전처리
         processed_data, dataframe = pipeline.preprocess_data(data)
-        logger.info(f"\'{sensor_id}\': Data preprocessing completed.")
+        logger.info(f"[\'{sensor_id}\' at {time}] Data preprocessing completed.")
 
         # 비정상 데이터(용존산소 3 이하)인 경우 모델 실행 중단 및 비정상으로 저장
         if dataframe['oxygen_ppm'].iloc[-1] <= 3.0:
-            logger.info(f"\'{sensor_id}\': Abnormal data, no prediction.")
-            await database.saveResult(sensor_id, None, 1)
+            logger.info(f"[\'{sensor_id}\' at {time}] Abnormal data, no prediction.")
+            await database.saveResult(sensor_id, time, None, 1)
             return
 
         # 모델 로드
@@ -59,17 +61,31 @@ async def runModel(sensor_id: str, time: datetime):
 
         # 예측
         result = pipeline.predict_anomaly(model, processed_data, device)  
-        logger.info(f"\'{sensor_id}\': Prediction has been successfully completed.")
+        logger.info(f"[\'{sensor_id}\' at {time}] Prediction successfully completed.")
 
         # 결과 저장
-        await database.saveResult(sensor_id, 1 if result == 1 else 0)
+        await database.saveResult(sensor_id, time, 1 if result == 1 else 0)
 
     except Exception as e:
         logger.error(str(e), exc_info=True)
-        logger.info("Prediction for \'{sensor_id}\' failed.")
-        await database.saveResult(sensor_id, None, None)
+        logger.info(f"[\'{sensor_id}\' at {time}] Prediction failed.")
+        await database.saveResult(sensor_id, time, None, None)
 async def runModelWrapper(sensor_id: str, time: datetime):
-    await runModel(sensor_id, time)
+    try: 
+        await asyncio.wait_for(runModel(sensor_id, time), 30)
+    except asyncio.TimeoutError:
+        logger.error("Task timed out.")
+
+
+# 과거 기록된 데이터에 대해서 모델 실행 및 결과 기록
+async def populatePastData(sensor_id: str, start_time: datetime, end_time: datetime):
+    try:
+        time = start_time
+        while time <= end_time:
+            await asyncio.wait_for(runModel(sensor_id, time), 30)
+            time += timedelta(hours=1)
+    except asyncio.TimeoutError:
+        logger.error("Task timed out.")
 
 
 # 서버 시작
@@ -77,20 +93,28 @@ async def runModelWrapper(sensor_id: str, time: datetime):
 async def startService():
     try:
         await database.initialize()
-
         # 1시간마다 모든 센서에 대해 모델을 실행하도록 스케줄링
         scheduler = AsyncIOScheduler(executors={"default": AsyncIOExecutor()})
-        # trigger = CronTrigger(minute="*/1")   # for test
         trigger = CronTrigger(minute="0")   # 정각(##:00)마다 실행
         for sensor_id in SENSOR_IDS:
             scheduler.add_job(
-                runModelWrapper, trigger, args=[sensor_id, datetime.now()], id=f"job_{sensor_id}", max_instances=3
+                runModelWrapper, trigger, args=[sensor_id, None], id=f"job_{sensor_id}", max_instances=3
             )
         scheduler.start()
         logger.info("Scheduler started.")
-
     except Exception as e:
         logger.error(str(e), exc_info=True)
+
+
+# 기존 테이블에 저장된 데이터 삭제
+@app.delete("/clear")
+async def clear():
+    try:
+        await database.clearResults()
+        logging.info("All recorded results have been cleared.")
+        return {"status": "success"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to clear results: {str(e)}")
     
 
 class Result(BaseModel):
@@ -105,6 +129,8 @@ async def latestResult(sensor_id: str):
         result = await database.getLatestResult(sensor_id)
         logger.info(f"Fetched the latest result for \'{sensor_id}\'")
         return Result(**result)
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid sensor id or no result")
     except Exception as e:
         logger.error(f"An error occured fetching latest result for \'{sensor_id}\': {e}", exc_info=True)
         raise HTTPException(status_code=500, detail="Internal Server Error")
@@ -117,19 +143,25 @@ async def resultsByTime(sensor_id: str, start_time: str, end_time: str):
         results = await database.getResultsByTime(sensor_id, start_time, end_time)
         logger.info(f"Fetched results for \'{sensor_id}\' from {start_time} to {end_time}.")
         return [Result(**result) for result in results]
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid sensor id or no result")
     except Exception as e:
         logger.error(f"An error occured fetching results for \'{sensor_id}\': {e}", exc_info=True)
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
-# 오든 센서 데이터 API에 대한 Proxy API
-@app.get("/proxy/{endpoint}")
-def proxy_request(endpoint: str, params: dict = None):
-    headers = {"Content-Type": "application/json"}
-    url = f"{os.getenv('API_BASE_URL')}/{endpoint}"
+@app.post("/populate/{sensor_id}")
+async def populate(
+        background_tasks: BackgroundTasks,
+        sensor_id: str,
+        start_time: datetime,
+        end_time: Optional[datetime]=None
+    ):
     try:
-        response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()
-        return response.json()
-    except requests.RequestException as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        if end_time is None:
+            end_time = datetime.now()
+        background_tasks.add_task(populatePastData, sensor_id, start_time, end_time)
+        return {"status": "accepted"}
+    except Exception as e:
+        logger.error(f"An error occured populating past results for \'{sensor_id}\': {e}", exc_info=True)
+        raise HTTPException(status_code=500, detail=f"Failed to populate past results: {str(e)}")
